123ee61a8bdea975fce14127561400e8c6898651
Gerber, Mike <mike.gerber@sbb.spk-berlin.de>
2020-02-13 16:04:17 +0100

    v0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ocrd_calamari',
-    version='0.0.5',
+    version='0.0.6',
     description='Calamari bindings',
     long_description=Path('README.md').read_text(),
     long_description_content_type='text/markdown',
