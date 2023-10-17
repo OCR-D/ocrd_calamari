96ae14ca5cd4f4f5ea835c3b8fdf020f6d75ef35
Mike Gerber <mike.gerber@sbb.spk-berlin.de>
2023-10-17 22:25:17 +0200

    🐛 Fix importing + exporting test.base.assets

--- test/base.py
+++ test/base.py
@@ -3,9 +3,12 @@
 import os
 import sys
 
+from test.assets import assets
 from ocrd_utils import initLogging
 
 PWD = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(PWD + "/../ocrd")
 
 initLogging()
+
+__all__ = ["assets"]
