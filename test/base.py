dca61c08c364acab2ddd16ee48a8fd9485c28ecb
Mike Gerber <mike.gerber@sbb.spk-berlin.de>
2023-10-18 21:39:05 +0200

    🎨 Sort remaining import blocks using ruff

--- test/base.py
+++ test/base.py
@@ -2,12 +2,12 @@
 
 import os
 import sys
-
 from test.assets import assets
+
+from ocrd_utils import initLogging
 
 PWD = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(PWD + "/../ocrd")
-from ocrd_utils import initLogging
 
 initLogging()
 
