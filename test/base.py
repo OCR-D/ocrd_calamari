dde50ae17a3f295aba6e8515bd7a53ffddb16f77
Mike Gerber <mike.gerber@sbb.spk-berlin.de>
2023-10-19 19:24:54 +0200

    🧹 Remove unused sys.path stuff from test.base

--- test/base.py
+++ test/base.py
@@ -1,13 +1,8 @@
 # pylint: disable=unused-import
 
-import os
-import sys
 from test.assets import assets
 
 from ocrd_utils import initLogging
-
-PWD = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(PWD + "/../ocrd")
 
 initLogging()
 
